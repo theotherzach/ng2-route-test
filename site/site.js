ecf29c2e25f9904c2de7ad6e3b785e58a978a6cd
Zach <briggszj@gmail.com>
2015-06-04 01:13:07 -0500

    Inject router

--- site/site.js
+++ site/site.js
@@ -1,16 +1,18 @@
-function SiteComponent() {
+function SiteComponent(router) {
+  console.log(router)
   this.message = "I'm the parent";
 }
+SiteComponent.parameters = [[angular.router.RouteConfig]]
 SiteComponent.annotations = [
   new angular.ComponentAnnotation({
-    selector: "site"
+    selector: "site",
+    injectables: [angular.router.RouteConfig]
   }),
   new angular.ViewAnnotation({
     templateUrl: 'site/site.html',
-    // directives: [ChildComponent]
+    directives: [HeaderComponent, FooterComponent]
   })
 ];
-
 
 document.addEventListener('DOMContentLoaded', function() {
   angular.bootstrap(SiteComponent);
